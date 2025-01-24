770969bc68405315cb4b951f1e7ccc202a197561
azkaghafoor <azkaghafoor17@gmail.com>
2025-01-24 12:34:17 -0800

    updated file

---
+++ variableDataType.js
@@ -0,0 +1,17 @@
+// Example
+const product = {
+        name: "Ball Pen",
+        rating: 4,
+        offer: 5,
+        price: 270,
+};
+console.log(product);
+
+// 2nd Example
+const profile = {
+    name: "Azka Ghafoor",
+    isFollow: false,
+    followers: 132,
+    following: 123,
+};
+console.log(profile);
