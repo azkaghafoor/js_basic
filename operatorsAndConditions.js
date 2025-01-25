fb01b8f003b79aecec7e8cc58b4d7a3569db649a
azkaghafoor <azkaghafoor17@gmail.com>
2025-01-25 06:42:58 -0800

    operators and conditionals statement done

---
+++ operatorsAndConditions.js
@@ -0,0 +1,158 @@
+// console.log("Hello World!");
+// // Arithmetic Operators
+// // Plus Operator
+// console.log("Plus Operator");
+// let a = 5;
+// let b = 4;
+// console.log("a = ", a, " & b = ", b);
+// console.log("a + b =", a + b);
+// // Minus Operator
+// console.log("Minus Operator");
+// let a1 = 9;
+// let b1 = 4;
+// console.log("a1 = ", a1, " & b1 = ", b1);
+// console.log("a1 - b1 =", a1 - b1);
+// // Multiply Operator
+// console.log("Multiply Operator");
+// let a2 = 3;
+// let b2 = 2;
+// console.log("a2 = ", a2, " & b2 = ",b2);
+// console.log("a2 * b2 =", a2 * b2);
+// // Divide Operator
+// console.log("Divide Operator");
+// let a3 = 5;
+// let b3 = 50;
+// console.log("a3 = ", a3, " & b3 = ", b3);
+// console.log("a3 / b3 =", a3 / b3);
+// // Modulus Operator
+// console.log("Modulus Operator");
+// let a4 = 6;
+// let b4 = 8;
+// console.log("a4 = ", a4, " & b4 = ", b4);
+// console.log("a4 % b4", a4 % b4);
+// // Exponentiation Operator
+// console.log("Exponentiation Operator");
+// let a5 = 3;
+// let b5 = 4;
+// console.log("a5 = ", a5, " & b5 = ", b5);
+// console.log("a5 ** b5", a5 ** b5);
+// // Unary Operators
+// // Increment Operator
+// console.log("Increment Operator");
+// let a6 = 4;
+// let b6 = 6;
+// console.log("a6 = ", a6, "& b6 = ", b6);
+// a6 = a6 + 1;
+// console.log("a6 = ", a6);
+// // Decrement Operator
+// console.log("Decrement Operator");
+// let a7 = 3;
+// let b7 = 6;
+// console.log("a7 = ", a7, "& b7 =", b7);
+// b7 = b7 - 1;
+// console.log("b7 = ", b7);
+
+
+// Assignment Operators
+// let a = 5;
+// let b = 2;
+// a += 4;    // (a = a + 4)
+// console.log("a =", a);
+
+// a -= 4;    // (a = a - 4)
+// console.log("a =", a);
+
+// a *= 4;    // (a = a * 4)
+// console.log("a =", a);
+
+// a /= 4;    // (a = a / 4)
+// console.log("a =", a);
+
+
+// Comparison Operator
+// let a = 6;
+// let b = 5;
+
+// console.log("a == b", a == b);
+// console.log("a != b", a != b);
+// console.log("a === b", a === b);
+// console.log("a !== b", a !== b);
+
+// console.log("a <= b", a <= b);
+
+// Logical Operators
+// logical And
+// let a = 6;
+// let b = 5;
+// let cond1 = a < b;
+// let cond2 = a === 6;
+// console.log("cond1 && cond2 =", cond1 && cond2);
+
+// Logical Or
+// let a = 6;
+// let b = 5;
+// let cond1 = a < b;
+// let cond2 = a === 6;
+// console.log("cond1 || cond2 =", cond1 || cond2);
+
+// Logical Not
+// let a = 6;
+// let b = 5;
+// console.log("!(6<5) = ", !(a < b));
+
+
+// Conditional Statements
+// let mode ="blue";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// }
+// else{
+//     color = "white";
+// }
+// console.log(color);
+// let age = 25;
+// if (age >= 18){
+//     console.log("You Can Vote")
+// }
+// else{
+//     console.log("You Cannot Vote")
+// }
+// if (age >= 18){
+//     console.log("You Can Vote")
+// }
+
+// if (age < 18){
+//     console.log("You Cannot Vote")
+// }
+
+//Even And Odd
+// let num = 7;
+// if (num % 2 === 0){
+//     console.log(num,"is even");
+// }
+// else{
+//     console.log(num,"is odd");
+// }
+
+// Else-if Statement
+// let mode =  "silver";
+// let color;
+// if (mode === "dark"){
+//     color = "black";
+// }
+// else if (mode === "pink"){
+//     color = "pink";
+// }
+// else if (mode === "blue"){
+//     color = "blue";
+// }
+// else{
+//     color = "white";
+// }
+// console.log(color);
+
+// Ternary Operators
+// let age = 25;
+// let result = age>= 18 ? "adult": "not adult";
+// console.log(result);    //simpler, compact if-else
