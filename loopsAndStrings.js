62d67a08e0b26b8cf9bd621d363bb8f67a6f86a6
azkaghafoor <azkaghafoor17@gmail.com>
2025-01-25 18:42:56 -0800

    loops And Strings Lecture complete done

---
+++ loopsAndStrings.js
@@ -0,0 +1,91 @@
+// For Loop
+// for (let count= 1 ; count <= 100; count++){
+//     console.log("Apna College");
+// }
+// console.log("loop has ended");
+
+// // Calculate sum of 1 to 5
+// let sum = 0;
+// for(let i =1 ; i <= 5 ; i++){
+//     sum = sum + i;
+// }
+// console.log(" sum = ", sum);
+// console.log("Loop has Ended");
+
+// While loop
+// let i =1;
+// while(i<=5){
+//     console.log("i =", i);
+//     i++;
+// }
+
+//do-while loop 
+// let i =1;
+// do{  
+//     console.log("i =", i);
+//     i++;
+//  }while(i<=5);
+
+// for-of loop
+// let str = "JavaScript";
+// let size =0;
+// for(let val  of str){
+//     console.log("val =", val);
+//     size++
+// }
+// console.log("string size =", size);
+
+// for-in loop
+// let student = {
+//     name : "Azka Ghafoor", 
+//     age : 20,
+//     cgpa: 7.5,
+//     isPass: true,
+// };
+// for (let key in student){
+//     console.log("key = ", key, "value =", student[key]);
+// }
+
+
+// String
+// let str = "AzkaGhafoor";
+// console.log(str[1]);
+
+// let obj = {
+//     item: "pen",
+//     price: 10, 
+// };
+// let output = `The cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+// Template Literals
+// let specialString = `This is template literal string ${1 + 2 + 3}`;
+// console.log(specialString);
+
+
+// Escape Character
+// let str = "Azka Ghafoor";
+// console.log(str.length);
+
+// Methods
+// let str ="Apna College";
+// str =str.toUpperCase();
+// console.log(str);
+// let str ="ApnaCollege";
+// str =str.toLowerCase();
+// console.log(str);
+// let str ="     Apna College  JS    ";
+// console.log(str.trim());
+// Slice str
+// let str ="0123445";
+// console.log(str.slice(1, 4));
+
+// let str1 ="Apna";
+// let str2 = "College";
+// let res = str1.concat(str2);
+// console.log(res);
+
+// let str ="Hello";
+// console.log(str.replace("H", "y"));
+
+let str ="IloveJS";
+console.log(str.charAt(0));
